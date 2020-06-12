c770290de00f4643bb17ce61602f6422942441ca
Federico Gimenez <federico.gimenez@gmail.com>
2020-06-12 11:57:20 +0200

    range query test

--- test/prometheus-api-client.ts
+++ test/prometheus-api-client.ts
@@ -1,5 +1,6 @@
 import { should } from 'chai';
 import { createLogger } from '@w3f/logger';
+import moment = require('moment');
 
 import { PrometheusAPIClient } from '../src/index';
 
@@ -10,15 +11,28 @@ const url = 'http://prometheus:9090';
 
 const cfg = { url, logger };
 const subject = new PrometheusAPIClient(cfg);
+const query = 'up';
 
 describe('Prometheus API Client', () => {
     it('should retrieve instant metrics', async () => {
-        const query = 'up'
         const input = { query };
 
         const result = await subject.instantQuery(input);
 
         result.status.should.eq('success');
         result.data.resultType.should.eq('vector');
+    });
+
+    it('should retrieve range metrics', async () => {
+        const start = moment().startOf('hour').format();
+        const end = moment().format();
+        const step = '15s';
+
+        const input = { query, start, end, step };
+
+        const result = await subject.rangeQuery(input);
+
+        result.status.should.eq('success');
+        result.data.resultType.should.eq('matrix');
     });
 });
