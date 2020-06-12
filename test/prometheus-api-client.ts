fa84aa4d25532f65b2b9380e65b2da35ea4dae8e
Federico Gimenez <federico.gimenez@gmail.com>
2020-06-12 12:34:36 +0200

    extended tests

--- test/prometheus-api-client.ts
+++ test/prometheus-api-client.ts
@@ -13,7 +13,16 @@ const cfg = { url, logger };
 const subject = new PrometheusAPIClient(cfg);
 const query = 'up';
 
+function delay(ms: number) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
 describe('Prometheus API Client', () => {
+    before(async () => {
+        // give prometheus some time to get metrics
+        await delay(6000);
+    });
+
     it('should retrieve instant metrics', async () => {
         const input = { query };
 
@@ -21,12 +30,19 @@ describe('Prometheus API Client', () => {
 
         result.status.should.eq('success');
         result.data.resultType.should.eq('vector');
+        result.data.result.length.should.gt(0);
+        result.data.result.forEach((item) => {
+            item.metric['__name__'].should.eq(query);
+            item.metric['instance'].should.eq('localhost:9090');
+            item.metric['job'].should.eq('prometheus');
+            item.value[1].should.eq('1');
+        });
     });
 
     it('should retrieve range metrics', async () => {
         const start = moment().utc().startOf('hour').format();
         const end = moment().utc().format();
-        const step = '15s';
+        const step = '1s';
 
         const input = { query, start, end, step };
 
@@ -34,5 +50,15 @@ describe('Prometheus API Client', () => {
 
         result.status.should.eq('success');
         result.data.resultType.should.eq('matrix');
+        result.data.result.length.should.gt(0);
+        result.data.result.forEach((item) => {
+            item.metric['__name__'].should.eq(query);
+            item.metric['instance'].should.eq('localhost:9090');
+            item.metric['job'].should.eq('prometheus');
+            item.values.length.should.gt(0);
+            item.values.forEach((value) => {
+                value[1].should.eq('1');
+            });
+        });
     });
 });
