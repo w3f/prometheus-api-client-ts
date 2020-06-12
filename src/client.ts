9dec499118cc060c050f88f3e5fc7f94973fc201
Federico Gimenez <federico.gimenez@gmail.com>
2020-06-12 11:10:55 +0200

    implement instant query

--- src/client.ts
+++ src/client.ts
@@ -1,4 +1,5 @@
 import { Logger, createLogger } from '@w3f/logger';
+import got from 'got';
 
 import {
     PrometheusAPIClientInterface,
@@ -16,7 +17,7 @@ export class PrometheusAPIClient implements PrometheusAPIClientInterface {
     private readonly logger: Logger;
 
     constructor(cfg: PrometheusClientConfig) {
-        this.url = cfg.url;
+        this.url = `${cfg.url}/api/v1`;
         this.headers = cfg.headers;
         if (!cfg.logger) {
             this.logger = createLogger();
@@ -26,7 +27,18 @@ export class PrometheusAPIClient implements PrometheusAPIClientInterface {
     }
 
     async instantQuery(input: InstantQueryInput): Promise<InstantResponse> {
-        return
+        let url = `${this.url}/query?query=${input.query}`;
+        if (input.time) {
+            url += `&time=${input.time}`;
+        }
+        if (input.timeout) {
+            url += `&timeout=${input.timeout}`;
+        }
+        this.logger.debug(`Sending instant query '${input.query}' to ${url}`);
+
+        const body = await got(url, { headers: this.headers }).json();
+
+        return body as InstantResponse;
     }
     async rangeQuery(input: RangeQueryInput): Promise<RangeResponse> {
         return
