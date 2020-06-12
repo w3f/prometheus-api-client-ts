6d5f3c7dba9b8a8266d70d618f20f6964b232e6d
Federico Gimenez <federico.gimenez@gmail.com>
2020-06-12 10:33:50 +0200

    empty implementation

--- src/client.ts
+++ src/client.ts
@@ -1,8 +1,34 @@
-import { Logger } from '@w3f/logger';
+import { Logger, createLogger } from '@w3f/logger';
 
+import {
+    PrometheusAPIClientInterface,
+    RequestHeaders,
+    InstantResponse,
+    RangeResponse,
+    PrometheusClientConfig,
+    InstantQueryInput,
+    RangeQueryInput
+} from './types';
 
-export class PrometheusClient {
-    constructor(
-        private readonly url: string,
-        private readonly logger: Logger) { }
+export class PrometheusAPIClient implements PrometheusAPIClientInterface {
+    private readonly url: string;
+    private readonly headers: RequestHeaders;
+    private readonly logger: Logger;
+
+    constructor(cfg: PrometheusClientConfig) {
+        this.url = cfg.url;
+        this.headers = cfg.headers;
+        if (!cfg.logger) {
+            this.logger = createLogger();
+        } else {
+            this.logger = cfg.logger;
+        }
+    }
+
+    async instantQuery(input: InstantQueryInput): Promise<InstantResponse> {
+        return
+    }
+    async rangeQuery(input: RangeQueryInput): Promise<RangeResponse> {
+        return
+    }
 }
