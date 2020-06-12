a620bad14e3347d8c7d4cfeeb0f8d4165ee1d88a
Federico Gimenez <federico.gimenez@gmail.com>
2020-06-12 09:52:48 +0200

    complete api response types

--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,43 @@
 import { Logger } from '@w3f/logger';
 
 
+export type Status = 'success' | 'error';
+
+export type ResultType = 'matrix' | 'vector' | 'scalar' | 'string';
+
+export type MetricField = '__name__' | 'job' | 'instance';
+
+export type Metric = {
+    [field in MetricField]: string;
+}
+
+export type Value = [number, string];
+
+export interface ResultItem {
+    metric: Metric;
+    value?: Value;
+    values?: Array<Value>;
+}
+
+export interface Data {
+    resultType: ResultType;
+    result: Array<ResultItem>;
+}
+
+export interface Response {
+    status: Status;
+    data: Data;
+
+    errorType?: string;
+    error?: string;
+    warnings?: Array<string>;
+}
+
+export interface PrometheusAPIClientInterface {
+    instantQuery(): Promise<Response>
+}
+
+
 export interface RequestHeaders {
     [key: string]: string;
 }
